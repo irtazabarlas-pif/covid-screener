5872d6ad67525fe667deb4649ff4136c510125dd
Irtaza Barlas <irtaza.barlas@gsa.gov>
2020-06-24 21:18:16 -0400

    Fixed fonts sizing in the CSS & Flowchart ID 6

--- Flowcharts.js
+++ Flowcharts.js
@@ -331,12 +331,12 @@ const flowcharts = [
             },
             "b1": {
                 "type": "bool_decision",
+                "short": "Symptoms to Watch?",
                 "text": `<div class='text-left'>Are you experiencing any symptoms of COVID-19: <br><br>
                         &#9830; coughing<br>&#9830; fever or chilss<br>&#9830; shortness of breath or difficulty breathing<br>
                         &#9830; fatigue<br>&#9830; headache
                         </div>`,
-                "theme": "white-card",
-                "short": "Symptoms to Watch?",
+                "theme": "white-card",                
                 "sms" : "Are you experiencing any symptoms of COVID-19 (e.g., coughing, fever or chills, shortness of breath or difficulty breathing, fatigue, headache)?",
                 "info": "These are the most common symptoms associated with Covid-19.",
                 "yes" : "b10",
@@ -344,7 +344,7 @@ const flowcharts = [
             },
             "b2": {
                 "type": "bool_decision",
-                "short": "Advised?",
+                "short": "Prior Advice?",
                 "theme": "white-card",
                 "text": "<div class='text-left'>In the last 14 days, have you received instructions from a public health authority to self-observe, self-isolate, or self-quarantine?</div>",
                 "sms" : "Within the past 14 days, have you received instruction from a public health or medical professional to self-monitor, self-isolate, or self-quarantine because of concerns about COVID-19 infection? This includes you receiving a COVID-19 test recommended by a medical professional, with results still pending. Please enter Yes or No",
@@ -366,7 +366,9 @@ const flowcharts = [
                 "type": "statement",
                 "short": "Screening Needed",
                 "theme": "cyan-card",
-                "text": "<span style='font-size:2.5em; line-height: 1.2em;'>More Screening Needed.</span><br>Valid for $DATETIME.<br>Please show this screen to security at the facility entrance.",
+                "text": `<span style='font-size:2.5em; line-height: 1.2em;'>More Screening Needed.</span>
+                        <hr><div style='color: yellow;background-color: blue;'>Valid for $DATETIME.</div>
+                        <hr>Please show this screen to security at the facility entrance.`,
                 "sms" : "Access to VA Building Not Approved.",
                 "info": "Your help in containing this epidemic is highly appreciate.",
                 "next": ""
@@ -377,7 +379,7 @@ const flowcharts = [
                 "theme": "blue-card",
                 "text": `<span style='font-size:50px; text-align: center;'>&#9745;</span><br>
                         <span style='font-size: 2em; line-height: 1.2em; text-align: left;'>Access to the VA Buildings 810/811 Vermont Ave Approved.</span>
-                        <br>Valid for $DATETIME.<br>Please show this screen to security at the facility entrance.`,
+                        <hr><span style='color: yellow;'>Valid for $DATETIME.</span><hr>Please show this screen to security at the facility entrance.`,
                 "sms" : "Access to the VA Buildings 810/811 Vermont Ave Approved.\nPlease show this screen to security at the facility entrance.",
                 "info": "You can enter the facilities but follow safety protocols and Social Distancing.",
                 "next": ""
